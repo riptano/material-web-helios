9ed795c196b65579916416766a82b36f0a1a3f14
Cole Goldsmith <cole.goldsmith@datastax.com>
2022-06-14 23:47:48 -0500

    use css variables instead for easier theming, add card styles

--- index.ts
+++ index.ts
@@ -1,5 +1,6 @@
 import { MDCTextField } from "@material/textfield";
 import { MDCTextFieldHelperText } from "@material/textfield/helper-text";
+import { MDCSwitch } from "@material/switch";
 
 const textFields = [].map.call(
   document.querySelectorAll(".mdc-text-field"),
@@ -13,7 +14,23 @@ const textFields = [].map.call(
 const helperTexts = [].map.call(
   document.querySelectorAll(".mdc-text-field-helper-text"),
   (el: any) => {
-    const textField = new MDCTextFieldHelperText(el);
-    return textField;
+    return new MDCTextFieldHelperText(el);
   }
 );
+
+const switches = [].map.call(
+  document.querySelectorAll(".mdc-switch"),
+  (el: any) => {
+    return new MDCSwitch(el);
+  }
+);
+
+const themeSwitch = document.getElementById("theme-switch");
+themeSwitch.onclick = () => {
+  const currentTheme = document.documentElement.getAttribute("data-theme");
+  let targetTheme = "light";
+  if (currentTheme === "light") {
+    targetTheme = "dark";
+  }
+  document.documentElement.setAttribute("data-theme", targetTheme);
+};
--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = [
     module: {
       rules: [
         {
-          test: /\.scss$/,
+          test: /\.?(s)css$/,
           use: [
             {
               loader: "file-loader",
